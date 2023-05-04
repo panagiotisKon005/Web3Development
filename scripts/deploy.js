ee0da8037307cb1812cbb6c5c97d16b64f8a1e74
Panagiotis Konstantinidis <p.konstantinidis@dataverse.gr>
2023-05-04 17:30:35 +0300

    Until Lesson7

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,5 +1,5 @@
 //imports
-const { ethers } = require("hardhat")
+const { ethers, run, network } = require("hardhat")
 
 
 //async main
@@ -11,7 +11,40 @@ async function main() {
     const simpleStorage = await SimpleStorageFactory.deploy()
     await simpleStorage.deployed()
     console.log(`Deployed contract to: ${simpleStorage.address}`)
+    
+    //Verification
+    if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
+      console.log("Waiting for block txes...")  
+      await simpleStorage.deployTransaction.wait(6) 
+      await verify(simpleStorage.address, [])
+    }
+
+    const currentValue = await simpleStorage.retrieve()
+    console.log(`Current Value is, ${currentValue}`)
+
+    // Update the current calue
+    const transactionResponse = await simpleStorage.store(7)
+    await transactionResponse.wait(1)
+    const updatedValue = await simpleStorage.retrieve()
+    console.log(`Updated Value is: ${updatedValue}`)
+  
   }
+
+async function verify(contractAddress, args) {  
+    console.log("Verifing Contract...")
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+          })
+    } catch (e) {
+      if (e.message.toLowerCase().includes("already verified")) {
+        console.log("Already Verified!")
+      } else {
+        console.log(e)
+      }
+    }
+}
 
 //main
 main()
